db7f0f386f18b011b14236141df18e45a6d91138
Catherine Price <catprice444@gmail.com>
2020-05-13 20:55:32 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,12 +1,12 @@
 class School
-  attr_reader :add_student
+  
   def roster
     roster = []
   end
 
-  def add_student=(name, grade)
-    @name = name
-    @grade = grade
-  end
+  # def add_student=(name, grade)
+  #   @name = name
+  #   @grade = grade
+  # end
 
 end
