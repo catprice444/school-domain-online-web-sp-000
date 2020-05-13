169ad0d2b9dd13605c4410f8f3e525dd5d5e9346
Catherine Price <catprice444@gmail.com>
2020-05-13 21:12:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -11,12 +11,19 @@ class School
     roster[level] << student
   end
 
-  def grade(level)
-    roster.sort do |x, y|
-      if x == level
-        return y
-      end 
-    end
+  # def grade(level)
+  #   roster.sort do |x, y|
+  #     if x == level
+  #       return y
+  #     end
+  #   end
+  def sort
+  nu_hash = {}
+  roster.each do |x, y|
+    nu_hash[x] = y.sort
+  end
+  nu_hash
+end 
   end
 
 
